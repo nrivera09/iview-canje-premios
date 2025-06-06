9722ceca45baa4b4cd530b552770a5902e46559c
Neill Bryan Rivera Livia <nrivera@acity.com.pe>
2025-06-06 16:58:28 -0500

    avance
    avance

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { useSoundEffect } from "./shared/hooks/useSoundEffect";
 import { useStockSignalR } from "./shared/hooks/useStockSignalR";
 import { usePrizesStore } from "./shared/store/prizesStore";
 import { useURLParams } from "./shared/hooks/useURLParams";
+import { ConsoleOverlay } from "./shared/components/ConsoleOverlay";
+import { handleCloseClick } from "./libs/altenarBridge";
 
 const App: React.FC = () => {
   useURLParams();
@@ -18,6 +20,7 @@ const App: React.FC = () => {
   const { playSound } = useSoundEffect();
 
   const [showInput, setShowInput] = useState(false);
+  const [showBug, setShowBug] = useState(false);
   const ready = useAppData(tarjetaId);
 
   useStockSignalR((data) => {
@@ -28,6 +31,15 @@ const App: React.FC = () => {
   const hideIviewHack = () => {
     setShowInput(false);
   };
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+      console.log("Click en:", target.tagName, target.className);
+    };
+    window.addEventListener("click", handleClick);
+    return () => window.removeEventListener("click", handleClick);
+  }, []);
 
   if (!ready) {
     return (
@@ -40,9 +52,28 @@ const App: React.FC = () => {
   return (
     <>
       <Router />
-
+      <button
+        onClick={() => {
+          playSound("button");
+          handleCloseClick();
+        }}
+        className="z-50 font-bold text-xl bg-red-600 hover:bg-red-900 p-1 rounded-md overflow-hidden   mx-auto transition-all cursor-pointer hover:shadow-xl h-[30px] sm:h-[30px] w-[30px] sm:w-[30px] items-center justify-center absolute top-0 right-0 mt-[50px] mr-1"
+      >
+        CERRAR APP
+      </button>
+      <div className="absolute top-1/2 right-0 mr-1 flex flex-col items-end gap-2">
+        <button
+          onClick={() => {
+            playSound("button");
+            setShowBug(!showBug);
+          }}
+          className="btn  p-1 border-none hover:inset-0"
+        >
+          BUG
+        </button>
+      </div>
       {isDevEnv && (
-        <div className="absolute top-1/2 right-0 mr-1 flex flex-col items-end gap-2">
+        <div className="!hidden absolute top-1/2 right-0 mr-1 flex flex-col items-end gap-2">
           <button
             onClick={() => {
               playSound("button");
@@ -56,6 +87,7 @@ const App: React.FC = () => {
       )}
 
       {showInput && <HackIview hideIviewHack={hideIviewHack}></HackIview>}
+      {showBug && <ConsoleOverlay></ConsoleOverlay>}
     </>
   );
 };
